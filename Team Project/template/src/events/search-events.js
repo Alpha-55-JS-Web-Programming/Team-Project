edfe552156269a8482d74fa33d63abdf8d2e6f05
AndreyRaychev <andreyraychev27@gmail.com>
2024-01-18 17:19:58 +0200

    add events/search-events.js

--- Team Project/template/src/events/search-events.js
+++ Team Project/template/src/events/search-events.js
@@ -1,4 +1,9 @@
+import { CONTAINER_SELECTOR } from '../common/constants.js';
+import { loadSearchMovies } from '../requests/request-service.js';
+import { toSearchView } from '../views/search-view.js';
+import { q } from './helpers.js';
 
 export const renderSearchItems = (searchTerm) => {
-  // missing implementation
+  loadSearchMovies(searchTerm)
+    .then(gifs => q(CONTAINER_SELECTOR).innerHTML = toSearchView(gifs, searchTerm));
 };
