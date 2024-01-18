3767961838424dd181e7ca2c16b74f6bd26970ea
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-01-18 23:53:47 +0200

    trending view changed

--- Team Project/template/src/events/navigation-events.js
+++ Team Project/template/src/events/navigation-events.js
@@ -54,7 +54,7 @@ export const viewGifDetails = async (getTrendingUrl) => {
 // console.log(event);
 // console.log(event.target);
 // 1 . await GET GIF ID
-// 2. q(CONTAINER_SELECTOR).innerHTML = displayGifInfo(GET GIF ID);
+// 2. q(CONTAINER_SELECTOR).innerHTML = displayGifDetails(GET GIF ID);
   try {
     const response = await fetch(getTrendingUrl);
     console.log(response);
