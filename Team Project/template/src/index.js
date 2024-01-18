dd6eec2e70eb68adb0e5200e640b4dff8e583008
Martin Andreev <xxdead_worldxx@abv.bg>
2024-01-18 19:49:06 +0200

    Edint trending related variables

--- Team Project/template/src/index.js
+++ Team Project/template/src/index.js
@@ -1,7 +1,7 @@
 import { HOME } from './common/constants.js';
 import { toggleFavoriteStatus } from './events/favorites-events.js';
 import { q } from './events/helpers.js';
-import { loadPage, renderCategory, renderMovieDetails } from './events/navigation-events.js';
+import { loadPage } from './events/navigation-events.js'; // change
 import { renderSearchItems } from './events/search-events.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,23 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // nav events
     if (event.target.classList.contains('nav-link')) {
-
       loadPage(event.target.getAttribute('data-page'));
     }
 
-    // show category events
-    if (event.target.classList.contains(/* your button class here */)) {
-      renderCategory(+event.target.getAttribute(/* your correct data attribute here */));
+    // show trending events
+    if (event.target.classList.contains('trending-link')) { // change
+      renderTrending(+event.target.getAttribute('data-trending')); // change
     }
 
     // show movie events
-    if (event.target.classList.contains(/* your button class here */)) {
-      renderMovieDetails(+event.target.getAttribute(/* your correct data attribute here */));
+    if (event.target.classList.contains('movie-link')) {
+      renderMovieDetails(+event.target.getAttribute('data-movie'));
     }
 
     // toggle favorite event
-    if (event.target.classList.contains(/* favorites item class here */)) {
-      toggleFavoriteStatus(+event.target.getAttribute(/* your correct data attribute here */));
+    if (event.target.classList.contains('favorite')) {
+      toggleFavoriteStatus(+event.target.getAttribute('data-movie-id'));
     }
 
   });
