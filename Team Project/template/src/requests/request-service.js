554b7fffdf0ad25be035b6e04f276520026734c3
Martin Andreev <xxdead_worldxx@abv.bg>
2024-01-18 19:30:59 +0200

    Edit request-service.js

--- Team Project/template/src/requests/request-service.js
+++ Team Project/template/src/requests/request-service.js
@@ -1,12 +1,26 @@
 // import { getCategories, getMoviesGeneralInfo, getMovieById, getCategory, searchMovies } from '../data/movies.js';
-import {getTrendingUrl} from '../common/constants.js'
+import { getTrendingUrl } from '../common/constants.js'
 
-export const loadSearchMovies = (searchTerm = '') => {
-  // missing implementation
-};
-
+/**
+*
+* @returns {Promise<Array<{
+  * id: string,
+  * rating: string,
+  * title: string,
+  * images: {
+  *  fixed_width: {
+  *   url: string,
+  *  },
+  * },
+  * user: {
+  *  avatar_url: string,
+  *  username: string,
+  * },
+  * }>>} - array of trending gifs
+  */
 export const loadTrendingGifs = async () => {
-  const response = await fetch(getTrendingUrl(25, 0))
-  const result = await response.json()
-  return result.data;
+  const response = await fetch(getTrendingUrl(30));
+  const result = await response.json();
+
+  return result.data
 };
