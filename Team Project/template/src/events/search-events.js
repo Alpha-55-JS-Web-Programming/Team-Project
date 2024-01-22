7de3314143b6054ccbf179bfc4738833c62d4c51
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-01-22 13:02:59 +0200

    formatted document

--- Team Project/template/src/events/search-events.js
+++ Team Project/template/src/events/search-events.js
@@ -3,7 +3,7 @@ import { loadSearchResults } from '../requests/request-service.js';
 import { toTrendingView } from '../views/trending-view.js';
 import { q } from './helpers.js';
 
-export const renderSearchItems = async(searchTerm) => {
-  const search = await loadSearchResults(searchTerm)
+export const renderSearchItems = async (searchTerm) => {
+  const search = await loadSearchResults(searchTerm);
   q(CONTAINER_SELECTOR).innerHTML = toTrendingView(search);
 };
