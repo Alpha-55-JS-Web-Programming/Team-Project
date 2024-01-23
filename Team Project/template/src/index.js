a10340f9bc937c5b735b03084e6b0336c9e11fa1
Martin Andreev <xxdead_worldxx@abv.bg>
2024-01-23 20:48:55 +0200

    Return the code to previous state due to visualizisation issue

--- Team Project/template/src/index.js
+++ Team Project/template/src/index.js
@@ -6,49 +6,44 @@ import { renderSearchItems } from "./events/search-events.js";
 import { initiateUpload } from "./events/navigation-events.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // add global listener
-  document.addEventListener("click", (event) => {
-    // nav events
-    if (event.target.classList.contains("nav-link")) {
-      loadPage(event.target.getAttribute("data-page"));
-    }
+    // add global listener
+    document.addEventListener("click", (event) => {
+        // nav events
+        if (event.target.classList.contains("nav-link")) {
+            loadPage(event.target.getAttribute("data-page"));
+        }
 
-    // show movie events
-    if (event.target.classList.contains("view-gif-details-button")) {
-      viewGifDetails(event.target.getAttribute("data-gif-id"));
-    }
+        // show movie events
+        if (event.target.classList.contains("view-gif-details-button")) {
+            viewGifDetails(event.target.getAttribute("data-gif-id"));
+        }
 
-    // upload
-    // listening for click event from upload-view
-    if (event.target.classList.contains("upload-btn")) {
-      initiateUpload();
-    }
+        // upload
+        // listening for click event from upload-view
+        if (event.target.classList.contains("upload-btn")) {
+            initiateUpload();
+        }
 
-    // toggle favorite event
-    if (event.target.classList.contains("favorite")) {
-      toggleFavoriteStatus(event.target.getAttribute("data-gif-id"));
-    }
+        // toggle favorite event
+        if (event.target.classList.contains("favorite")) {
+            toggleFavoriteStatus(event.target.getAttribute("data-gif-id"));
+        }
 
-    if (event.target.classList.contains("button-picture")) {
-      console.log('click');
-      console.log(document.getElementById("search"));
-      renderSearchItems(document.getElementById("search").value);
-    }
+        if (event.target.classList.contains("button-picture")) {
+            console.log('click');
+            console.log(document.getElementById("search"));
+            renderSearchItems(document.getElementById("search").value);
+        }
 
-  });
+    });
 
-  // search events
-  // q("input#search").addEventListener("input", (event) => {
-  //   renderSearchItems(event.target.value);
-  // });
-  q("input#search").addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-    renderSearchItems(event.target.value);
-    }
+    // search events
+    // q("input#search").addEventListener("input", (event) => {
+    //   renderSearchItems(event.target.value);
+    // });
+    q("input#search").addEventListener("input", (event) => {
+        renderSearchItems(event.target.value);
+    });
 
-
-
-  });
-
-  loadPage(HOME);
+    loadPage(HOME);
 });
