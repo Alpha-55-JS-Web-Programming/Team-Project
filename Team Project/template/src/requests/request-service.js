494b491373f6f912637e4f0addeef21a114f7f5b
Martin Andreev <xxdead_worldxx@abv.bg>
2024-01-24 10:29:46 +0200

    Set the JSDoc documentation for the request.service.js file.

--- Team Project/template/src/requests/request-service.js
+++ Team Project/template/src/requests/request-service.js
@@ -26,24 +26,86 @@ export const loadTrendingGifs = async () => {
 
 export const loadSingleGif = () => {};
 
+/**
+ *
+ * @param {string} gifId - The unique identifier for the GIF.
+ * @returns {Promise<{
+ *   id: string,
+ *   rating: string,
+ *   title: string,
+ *   images: {
+ *     fixed_width: {
+ *       url: string
+ *     },
+ *   },
+ *   user?: {
+ *     avatar_url: string,
+ *     username: string
+ *   }
+ * }>} A promise that resolves to an object containing details about the GIF.
+ */
 export const loadGifDetails = async (gifId) => {
   const response = await fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=${API_KEY}`);
   const result = await response.json();
+
   return result.data;
 };
 
+/**
+ *
+ * @param {string} searchInputValue - The search query string used to find GIFs.
+ * @returns {Promise<Array<{
+ *   id: string,
+ *   rating: string,
+ *   title: string,
+ *   images: {
+ *     fixed_width: {
+ *       url: string
+ *     }
+ *   },
+ *   user?: {
+ *     avatar_url: string,
+ *     username: string
+ *   }
+ * }>>} A promise that resolves to an array of GIF objects matching the search query.
+ */
 export const loadSearchResults = async (searchInputValue) => {
   const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchInputValue}&limit=25&offset=0&rating=g&lang=en`);
   const result = await response.json();
 
   return result.data;
 };
+
+/**
+ *
+ * @returns {Promise<{
+ *   id: string,
+ *   rating: string,
+ *   title: string,
+ *   images: {
+ *     fixed_width: {
+ *       url: string
+ *     }
+ *   },
+ *   user?: {
+ *     avatar_url: string,
+ *     username: string
+ *   }
+ * }>} A promise that resolves to an object containing details about the randomly selected GIF.
+ */
 export const loadRandomGif = async () => {
   const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
   const result = await response.json();
+
   return result.data;
 };
 
+/**
+ *
+ * @param {File} file - The GIF file to be uploaded.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating the success status of the upload operation.
+ * @throws {Error} Throws an error if the upload operation fails or the server responds with an unexpected status code.
+ */
 export const uploadGif = async (file) => {
   // creating FormData object to send key/value pairs via fetch() to a server
   const formData = new FormData();
@@ -68,11 +130,17 @@ export const uploadGif = async (file) => {
       }, ${await result.text()}`
     );
   }
+
   return result.ok;
 };
 
+/**
+ *
+ * @returns {Promise<string>} A promise that resolves to the ID of the uploaded GIF as a string, or an empty array if no GIF ID is stored.
+ */
 export const getGifUploadedId = async () => {
   // obtain the gif id from localStorage
   const uploadedGifId = localStorage.getItem("uploadedGifId") || [];
+
   return uploadedGifId;
 };
