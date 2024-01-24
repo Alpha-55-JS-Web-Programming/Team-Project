bbb79dc543958edd0fa285e74bee89a48e4e71b8
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-01-24 11:29:06 +0200

    date in about is not a magic number anymore

--- Team Project/template/src/common/constants.js
+++ Team Project/template/src/common/constants.js
@@ -15,3 +15,13 @@ export const ABOUT = "about";
 export const API_KEY = "1zuFE91nbybP5xGff9qAtw2N3j2zK7MF";
 
 export const CONTAINER_SELECTOR = "#container";
+
+const date = Date.now()
+const currentDate = new Date(date);
+
+export const date2 = currentDate.toLocaleDateString("en", {
+weekday: "short",
+year: "numeric",
+month: "2-digit",
+day: "numeric"
+})
--- Team Project/template/src/views/about-view.js
+++ Team Project/template/src/views/about-view.js
@@ -1,9 +1,11 @@
+import { date2 } from "../common/constants.js"
+
 export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>Web Team Project</h1>
     <h2>Authors: Team 6</h2>
-    <h2>Date: 24/01/2024</h2>
+    <h2>Date: ${ date2}</h2>
   </div>
 </div>
 `;
