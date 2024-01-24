f9103e164d9d81528236c491b76e4c0092bcd06f
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-01-24 11:29:51 +0200

    formatted consts

--- Team Project/template/src/common/constants.js
+++ Team Project/template/src/common/constants.js
@@ -16,12 +16,12 @@ export const API_KEY = "1zuFE91nbybP5xGff9qAtw2N3j2zK7MF";
 
 export const CONTAINER_SELECTOR = "#container";
 
-const date = Date.now()
+const date = Date.now();
 const currentDate = new Date(date);
 
 export const date2 = currentDate.toLocaleDateString("en", {
-weekday: "short",
-year: "numeric",
-month: "2-digit",
-day: "numeric"
-})
+    weekday: "short",
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric"
+});
