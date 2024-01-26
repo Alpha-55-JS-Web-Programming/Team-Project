694b56275c71494b5c60516298365a2ca824e815
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-01-26 15:44:43 +0200

    scrolling now shows another pile of gifs, not the same

--- Team Project/template/src/common/constants.js
+++ Team Project/template/src/common/constants.js
@@ -16,7 +16,7 @@ export const API_KEY = "1zuFE91nbybP5xGff9qAtw2N3j2zK7MF";
 
 export const getUrl =(limit = 30, offset)=>
 {
-    return `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${offset = offset + limit }`;
+    return `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${offset+limit }`;
 }
 
 export const CONTAINER_SELECTOR = "#container";
--- Team Project/template/src/scroll/scroll.js
+++ Team Project/template/src/scroll/scroll.js
@@ -1,49 +1,27 @@
 // import { q } from '../common/helpers.js';
-import { getUrl } from '../common/constants.js';
+// import { getUrl } from '../common/constants.js';
 import { toTrendingView } from '../views/trending-view.js';
 import { CONTAINER_SELECTOR } from '../common/constants.js';
-// import { API_KEY } from '../common/constants.js';
+import { API_KEY } from '../common/constants.js';
 
 
-// export const getUrl =(limit = 30, offset)=>{
-//     return `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${offset = offset + limit }`;
-// }
+let offset = 0; 
 
-export const handleScroll = async() => {
-    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight - 10) {
-        console.log('At the bottom');
-        const data = await loadInfiniteScroll();
-        document.querySelector(CONTAINER_SELECTOR).innerHTML += toTrendingView(data);
-        // append div ,to #container, with toTrendingView(data)
-        // q(CONTAINER_SELECTOR).append(toTrendingView(data));
-        // const postDiv = document.createElement('div');
-        // postDiv.className = 'post';
-        // postDiv.innerHTML = toTrendingView(data);
-        // q(CONTAINER_SELECTOR).append(postDiv);
+export const getUrl = (limit = 30) => {
+  return `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${offset+=limit}`;
+};
 
-    //     const postDiv = document.createElement('div');
-    //   postDiv.className = 'post';
-    //           const data = await loadInfiniteScroll();
-    //     document.getElementsByClassName('post').innerHTML = toTrendingView(data);
-    //     document.querySelector(CONTAINER_SELECTOR).append(postDiv);
+export const handleScroll = async () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 10) {
+    console.log('At the bottom');
+    const data = await loadInfiniteScroll();
+    document.querySelector(CONTAINER_SELECTOR).innerHTML += toTrendingView(data);
+  }
+};
 
-    }
-}
 export const loadInfiniteScroll = async () => {
-    const response = await fetch(getUrl(30,0));
-    const result = await response.json();
-  
-    return result.data;
-  };
-
-//   function appendDataToDOM(data) {
-//     data.forEach(post => {
-//       const postDiv = document.createElement('div');
-//       postDiv.className = 'post';
-//       postDiv.innerHTML = toTrendingView(data);
-//       contentDiv.appendChild(postDiv);
-//     });
-//   }
-
-  
+  const response = await fetch(getUrl(30));
+  const result = await response.json();
+  return result.data;
+};
