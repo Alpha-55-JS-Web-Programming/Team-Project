4fc2e1641f78f4b37a7a56649a11bab589209f70
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-04-03 12:54:11 +0300

    fix: size of photos in code add new img for readme.md

--- Team Project/template/src/views/about-view.js
+++ Team Project/template/src/views/about-view.js
@@ -4,8 +4,8 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>Web Team Project</h1>
-    <h2>Authors: Team 6</h2>
-    <h2>Date: ${ date2}</h2>
+    <h2>Authors: Andrey Raychev, Martin Andreev, Zvezda Neycheva</h2>
+    <h2>Date: ${date2}</h2>
   </div>
 </div>
 `;
