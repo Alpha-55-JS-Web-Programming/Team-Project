04293c7e02660910b8c539f84be3b03d4c88ea52
Martin Andreev <martin_i_a@abv.bg>
2025-04-15 17:33:37 +0300

    Replace dynamic date with fixed 'Project Completed' label in About page

--- src/common/constants.js
+++ src/common/constants.js
@@ -14,19 +14,12 @@ export const ABOUT = "about";
 
 export const API_KEY = "1zuFE91nbybP5xGff9qAtw2N3j2zK7MF";
 
-export const getUrl =(limit = 30, offset)=>
-{
-    return `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${offset+limit }`;
-}
+export const getUrl = (limit = 30, offset) => {
+  return `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}&offset=${
+    offset + limit
+  }`;
+};
 
 export const CONTAINER_SELECTOR = "#container";
 
-const date = Date.now();
-const currentDate = new Date(date);
-
-export const date2 = currentDate.toLocaleDateString("en", {
-    weekday: "short",
-    year: "numeric",
-    month: "2-digit",
-    day: "numeric"
-});
+export const date = "April, 2024";
--- src/views/about-view.js
+++ src/views/about-view.js
@@ -1,11 +1,11 @@
-import { date2 } from "../common/constants.js"
+import { date } from "../common/constants.js";
 
 export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>Web Team Project</h1>
     <h2>Authors: Andrey Raychev, Martin Andreev, Zvezda Neycheva</h2>
-    <h2>Date: ${date2}</h2>
+    <h2>Project Completed: ${date}</h2>
   </div>
 </div>
 `;
