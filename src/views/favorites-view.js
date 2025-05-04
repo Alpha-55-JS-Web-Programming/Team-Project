4f4c45e927ffdca789dd7c4da63ef24950705334
Martin Andreev <martin_i_a@abv.bg>
2025-05-04 21:22:22 +0300

    Change the CSS of the title shown in empty favorite page

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -5,10 +5,13 @@ import { toTrendingItemView } from "./trending-view.js";
  * @returns {Promise<string>} A promise that resolves to a string containing the HTML structure for displaying the favorite GIF.
  */
 export const toFavoritesView = (trendingGifs, hasFavorites) => {
-
   return `
     <section class="trending">
-      ${!hasFavorites ? "<b>No favorite GIFs have been added yet.</b>" : ""}
+    ${
+      !hasFavorites
+        ? '<div class="no-favorites-title"><b>No favorite GIFs have been added yet.</b></div>'
+        : ""
+    }
       <div class="trending-gifs">
         ${trendingGifs.map(toTrendingItemView).join("")}
       </div>
